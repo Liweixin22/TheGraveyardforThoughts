af0f78d1526c13e51fe20bb227813d0253b59ba8
liweixin <liweixin@cvte.com>
2025-02-24 09:20:01 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Lee's Blog";
-export const SITE_DESCRIPTION = "Welcome to my life!";
+export const SITE_TITLE = "The Graveyard for Thoughts";
+export const SITE_DESCRIPTION = "Welcome to The Graveyard for Thoughts!";
