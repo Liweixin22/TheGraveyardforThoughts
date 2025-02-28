87b5fd754ce3f1f8e09cdfbbc841b9c9934a2814
liweixin <liweixin@cvte.com>
2025-02-28 17:37:40 +0800

    replace react-dom/server.browser by react-dom/server.edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,22 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap(), react()],
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
